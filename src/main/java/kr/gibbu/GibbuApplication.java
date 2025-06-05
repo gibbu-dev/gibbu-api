0ea0eda8a95fa3f062952a779c9abb0c989052b6
YongBum <dt10002@naver.com>
2025-06-05 17:24:58 +0900

    Init: 프로젝트 초기 설정 셋팅
    - 프로젝트 초기 설정을 셋팅했습니다.
    - 기본적인 의존성을 build.gradle 파일에 추가했습니다.
    - 공통, 로컬, 운영 환경을 분리하여 application-{profile}.yml 파일을 작성했습니다.
    - application-{profile}.yml 파일의 환경변수 값들은 .env 파일로부터 가져옵니다.
    - 환경변수 값들은 노출이 되지 않도록 .gitignore 파일에 추가했습니다.
    
    issue #1

---
+++ src/main/java/kr/gibbu/GibbuApplication.java
@@ -0,0 +1,13 @@
+package kr.gibbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibbuApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GibbuApplication.class, args);
+  }
+
+}
